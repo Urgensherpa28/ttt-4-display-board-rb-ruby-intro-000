401cd3bd2e4be3ca320d70ed480fb83983732634
Urgen Sherpa <urgen.nyc@gmail.com>
2020-01-08 16:15:00 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-board = [" "," "," "," "," "," "," "," "," "]
+board = ["0","1","2","3","4","5","6","7","8"]
 def display_board(board)
   puts "   |   |   "
   puts "-----------"
