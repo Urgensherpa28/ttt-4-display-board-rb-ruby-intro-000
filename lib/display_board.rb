5f15448585adb46a8cadae34ffa554ec20975034
Urgen Sherpa <urgen.nyc@gmail.com>
2020-01-08 16:22:54 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
 
 board = ["0","1","2","3","4","5","6","7","8"]
 def display_board(board)
-  puts "   |   |   "
+  puts "   | x |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
